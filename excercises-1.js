9751f96a502d0f9c1de19225f4064d91b83083fe
Denia Camacho <deniacamacho04@gmail.com>
2019-10-08 17:10:57 -0700

    Done

--- excercises-1.js
+++ excercises-1.js
@@ -31,7 +31,9 @@
 // Q1
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
-
+function sum(a,b) {
+    return a+b 
+}
 console.assert(sum(2,4) === 6)
 console.assert(sum(45, 23) === 68)
 console.assert(sum(-2, -57) === -59)
@@ -39,7 +41,9 @@ console.assert(sum(-2, -57) === -59)
 // Q2
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
-
+function avg(a,b,c){
+    return (a+b+c) / 3
+}
 console.assert(avg(2,3,4) === 3)
 console.assert(avg(234, 63, 234) === 177)
 console.assert(avg(123,234,345) === 234)
@@ -47,26 +51,34 @@ console.assert(avg(123,234,345) === 234)
 // Q3
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
+function getLength (str){
+    return str.length
+}
 
 console.assert(getLength('Michael') === 7)
 console.assert(getLength('Javascript') === 10)
 console.assert(getLength('supercalifragilisticexpialidocious') === 34)
 
-// Q4
-// Write a function called `greaterThan` that takes
-// two parameters and returns `true` if the first
-// parameter is greater than the second. Otherwise
-// the function should return `false`.
-
+// // Q4
+// // Write a function called `greaterThan` that takes
+// // two parameters and returns `true` if the first
+// // parameter is greater than the second. Otherwise
+// // the function should return `false`.
+function greaterThan( a , b){
+    return a > b
+}
 console.assert(greaterThan(2, 1) === true)
 console.assert(greaterThan(23, 23) === false)
 console.assert(greaterThan(-234, 1) === false)
 
-// Q5
-// Write a function called `greet` that takes a
-// single parameter and returns a string that
-// is formated like "Hello, Name!" where *Name*
-// is the parameter that was passed in.
+// // Q5
+// // Write a function called `greet` that takes a
+// // single parameter and returns a string that
+// // is formated like "Hello, Name!" where *Name*
+// // is the parameter that was passed in.
+function greet(name){
+    return ("Hello, "+ name + "!")
+}
 
 console.assert(greet("Michael") === "Hello, Michael!")
 console.assert(greet("R2D2") === "Hello, R2D2!")
@@ -82,6 +94,9 @@ console.assert(greet("Iron Man") === "Hello, Iron Man!")
 // strings. For example:
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
+function madlib(str1,str2,str3,str4){
+    return `The ${str1} brown ${str2} ${str3} over the ${str4}.`
+}
 
 console.assert(madlib("quick","fox","jumps","fence")=== "The quick brown fox jumps over the fence.")
 console.assert(madlib("fat","rabbit","hops","wall")=== "The fat brown rabbit hops over the wall.")
--- excercises-2.js
+++ excercises-2.js
@@ -3,6 +3,10 @@
 // arguments and returns the largest of them. Use the 
 // if-then-else construct available in JavaScript.
 // ---------------------
+function max(num1, num2){
+    if (num1 < num2)
+   return num2
+}
 
 console.assert(max(2,3) === 3)
 console.assert(max(23,24) === 24)
@@ -12,7 +16,17 @@ console.assert(max(-23, 12) === 12)
 // Define a function maxOfThree() that takes three 
 // numbers as arguments and returns the largest of them.
 // ---------------------
-
+function maxOfThree(num1, num2, num3){
+    if (num1 > num2){
+        return num1
+    }
+   else if (num2 > num3){
+       return num2
+   }
+   else if(num3 > num1){
+       return num3
+   }
+}
 console.assert(maxOfThree(2,56,3) === 56)
 console.assert(maxOfThree(12,3,4) === 12)
 console.assert(maxOfThree(-12,4,-5) === 4)
@@ -22,6 +36,21 @@ console.assert(maxOfThree(-12,4,-5) === 4)
 // string of length 1) and returns true if it is a 
 // vowel, false otherwise.
 // ---------------------
+function isVowel(character){
+    if (
+        character=== 'a'||
+        character=== 'e'||
+        character=== 'i'||
+        character=== 'o'||
+        character=== 'u'||
+        character=== 'A'||
+        character=== 'E'||
+        character=== 'I'||
+        character=== 'O'||
+        character=== 'U')
+    { return true;
+    }else return false;
+    }
 
 console.assert(isVowel(0) === false);
 console.assert(isVowel("B") === false);
@@ -31,12 +60,27 @@ console.assert(isVowel("E") === true);
 console.assert(isVowel("2") === false)
 
 // ---------------------
-// Write a function rovarspraket() that will translate 
-// a text into "Rovarspraket". That is, double every 
-// consonant and place an occurrence of "o" in between. 
-// For example, translate("this is fun") should return 
-// the string "tothohisos isos fofunon".
-// ---------------------
+// // Write a function rovarspraket() that will translate 
+// // a text into "Rovarspraket". That is, double every 
+// // consonant and place an occurrence of "o" in between. 
+// // For example, translate("this is fun") should return 
+// // the string "tothohisos isos fofunon".
+// // ---------------------
+function rovarspraket(phrase){
+    var newStr= '' 
+        if (typeof phrase !== 'string'){
+            return phrase + ''
+    } else {
+        for (i = 0; i < phrase.length; i++) {
+        if (isVowel(phrase[i])) {
+            newStr += phrase[i]
+        } else {
+            newStr += phrase[i] + 'o' + phrase [i]
+        }
+         }
+    } return newStr
+}
+
 
 console.assert(rovarspraket("a") === "a")
 console.assert(rovarspraket("b") === "bob")
@@ -45,12 +89,13 @@ console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
 console.assert(rovarspraket(0) === "0")
 
 
-// ---------------------
-// Define a function reverse() that computes the reversal 
-// of a string. For example, reverse("jag testar") should 
-// return the string "ratset gaj".
-// ---------------------
-
-
+// // ---------------------
+// // Define a function reverse() that computes the reversal 
+// // of a string. For example, reverse("jag testar") should 
+// // return the string "ratset gaj".
+// // ---------------------
+function reverse(str){
+    return str.split ("").reverse().join("")
+}
 console.assert(reverse("books") === "skoob")
 console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
